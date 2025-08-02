7bfd2cc6b561ab3a6302692aceaecbc305ac0233
PaolaKuciauskas <paolakuciauskas@gmail.com>
2025-08-02 16:53:31 -0300

    version pao

--- maximo.py
+++ maximo.py
@@ -1,3 +1,11 @@
 # Cada uno haga su implementación de la función maximo la cual 
 # devuelve el máximo valor en una lista dada
 def maximo(lista):
+	if not lista:
+		return None  
+
+	max_valor = lista[0]
+	for numero in lista[1:]:
+		if numero > max_valor:
+			max_valor = numero
+	return max_valor
