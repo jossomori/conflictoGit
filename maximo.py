7e75403e005f6e4f544e9dd75bd63d68692974df
Josué Sosa <05josuesosa@gmail.com>
2025-08-02 17:03:54 -0300

    version de josue

--- maximo.py
+++ maximo.py
@@ -1,3 +1,16 @@
 # Cada uno haga su implementación de la función maximo la cual 
 # devuelve el máximo valor en una lista dada
 def maximo(lista):
+    n = len(lista)
+
+    # Caso borde
+    if n == 0:
+        print('La lista es vacía')
+        return None
+    
+    max = lista[0]
+    for i in range(1, n):
+        if lista[i] > max:
+            max = lista[i]
+
+    return max
