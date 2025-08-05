0c18948673ebec4478dd39c1a76d43eea3566fc2
Trilce <trilcegodoy11@gmail.com>
2025-08-05 17:33:14 -0300

    funcion maximo cambiada

--- maximo.py
+++ maximo.py
@@ -1,12 +1,4 @@
 # Cada uno haga su implementación de la función maximo la cual 
 # devuelve el máximo valor en una lista dada
 def maximo(lista):
-
-	if not lista:
-		return None  
-
-	max_valor = lista[0]
-	for numero in lista[1:]:
-		if numero > max_valor:
-			max_valor = numero
-	return max_valor
+	return max(lista) if lista else None
